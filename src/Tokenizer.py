2f6645dc96ca5c07cd032307cba94e9888e6ca0d
rominaoji <romina.oji.ro@gmail.com>
2022-01-07 14:48:26 +0330

    add word tokenizer

--- src/Tokenizer.py
+++ src/Tokenizer.py
@@ -6,6 +6,35 @@ class Tokenizer:
     def __init__(self):
         self.pattern_abbreviation = r"\b[آ-ی](?=([.]))(?:\1[آ-ی])*\b"
         self.tagger = POSTagger(model="resources/postagger.model")
+        self.punctuation_pattern = re.compile(r'([؟!\?]+|\d[\d\.:\/\\]+\d|[:\.،؛»\]\)\}"«\[\(\{])')
+        self.emoji_pattern = re.compile(
+            "(["
+            "\U00002500-\U00002BEF"  # chinese char
+            "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F700-\U0001F77F"  # alchemical symbols
+            "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+            "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+            "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+            "\U0001FA00-\U0001FA6F"  # Chess Symbols
+            "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+            "\U00002702-\U000027B0"  # Dingbats
+            "\U000024C2-\U0001F251"
+            "\U0001f926-\U0001f937"
+            "\U00010000-\U0010ffff"
+            "\u23f0"
+            "\u2640-\u2642"
+            "\u2600-\u2B55"
+            "\u200d"
+            "\u23cf"
+            "\u23e9"
+            "\u231a"
+            "\ufe0f"  # dingbats
+            "\u3030"
+            "])"
+        )
 
     def remove_dot_in_abbrevations(self, sentence):
         abbrevations = [x.group() for x in re.finditer(self.pattern_abbreviation, sentence)]
@@ -90,3 +119,11 @@ class Tokenizer:
                 sentences.append(temp)
                 temp = ''
         return sentences
+
+    def word_tokenize(self, sentence):
+        sentence = re.sub(self.punctuation_pattern, r' \1 ', sentence)
+        sentence = re.sub(self.emoji_pattern, r' \1 ', sentence)
+        sentence = sentence.replace('\n', ' ').replace('\t', ' ')
+        tokens = sentence.strip().split()
+        tokens = [x.strip("\u200c") for x in tokens if len(x.strip("\u200c")) != 0]
+        return tokens
