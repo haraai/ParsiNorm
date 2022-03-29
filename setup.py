1a0ae5b482bf0aa377a363df315fd9e74c72794b
rominaoji <romina.oji.ro@gmail.com>
2022-03-29 19:45:34 +0430

    add setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,10 @@ setuptools.setup(
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/haraai/ParsiNorm",
-     packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
-     install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk==3.6.5', 'hazm==0.7.0'],
+     install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk==3.3', 'hazm==0.7.0'],
  )
