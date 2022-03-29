b11034c5610a6a037a1bf767365ca91d1823e4cf
rominaoji <romina.oji.ro@gmail.com>
2022-03-29 19:34:10 +0430

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='parsinorm',  
+     version='0.0.1',
+     packages=['parsinorm'] ,
+     author="HaraAi",
+     author_email="info@hara.ai",
+     description="Persain Text Pre-Proceesing Tool",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/haraai/ParsiNorm",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=['num2fawords==1.1', 'persian-tools==0.0.10', 'urlextract==1.4.0', 'nltk==3.6.5', 'hazm==0.7.0'],
+ )
